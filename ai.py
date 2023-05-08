6aea0ee8ed6ef7afd19da31dad86396a83bc635f
X3NK <usachikdenys@gmail.com>
2023-05-08 18:24:20 +0300

    M3. Add new feature. Add file stickers.py.

--- ai.py
+++ ai.py
@@ -1,19 +1,43 @@
 import telebot
 import config
 import openai
+import stickers
 
 openai.api_key = (config.AIkey)
 bot = telebot.TeleBot(config.TOKEN)
 
-class Rec:
-    def airec(message):
+class AiRec:
+    def check_and_remove_prefix(variable):
+        if variable.startswith("."):
+            variable = variable[2:]
+        return variable
+    
+    def rec_name(message):
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"Виведи мені фільми схожі на: {message.text}, виведи тільки пронумерований список фільмів з назвою на анлійській мові та з роком виходу",
+            prompt=f"Виведи мені добірку фільмів схожих на: {message.text}, виведи тільки пронумерований список фільмів з назвою на анлійській мові та з роком виходу",
             temperature=0,
             max_tokens=1000,
             top_p=1.0,
             frequency_penalty=0.5,
             presence_penalty=0.0,
         )
-        bot.send_message(message.chat.id, text=response['choices'][0]['text'])
+        text=response['choices'][0]['text']
+        text = AiRec.check_and_remove_prefix(text)
+        bot.send_sticker(message.chat.id, stickers.sticker_fun)
+        bot.send_message(message.chat.id, f"Ось ваша добірка за схожим фільмом:\n{text}")
+
+    def rec_genre(message):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Виведи мені добірку фільмів за жанром: {message.text}, виведи тільки пронумерований список фільмів з назвою на анлійській мові та з роком виходу",
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.5,
+            presence_penalty=0.0,
+        )
+        text=response['choices'][0]['text']
+        text = AiRec.check_and_remove_prefix(text)
+        bot.send_sticker(message.chat.id, stickers.sticker_fun)
+        bot.send_message(message.chat.id, f"Ось ваша добірка за жанрами:\n{text}")
--- main.py
+++ main.py
@@ -6,9 +6,10 @@
 """
 
 
-# бібліотеки з токенами, а також з окремим функціями ШІ
+# бібліотеки з токенами, стікерами, а також з окремим функціями ШІ
 import config
 import ai
+import stickers
 # імпортуємо бібліотеки
 import telebot
 import sqlite3
@@ -24,7 +25,7 @@ bot = telebot.TeleBot(config.TOKEN)
 # створення об'єкту IMDb
 ia = IMDb()
 
-# під'єднуємось до бази даних
+# під'єднуємось до бази даних, якщо база даних існує то вказати шлях до неї
 conn = sqlite3.connect('C:\\Users\\usach\\Desktop\\CineBot\\database.db', check_same_thread=False)
 # створюємо курсор
 cursor = conn.cursor()
@@ -37,7 +38,7 @@ conn.commit()
 @bot.message_handler(commands=['start'])
 def start(message):
     # створюємо клавіатуру
-    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True, row_width = 3)
     # створюємо кнопки для клавіатури
     item1 = types.KeyboardButton('➕ Додати запис')
     item2 = types.KeyboardButton('📄 Показати записи')
@@ -45,11 +46,13 @@ def start(message):
     item4 = types.KeyboardButton('🎬 Топ-100 фільмів')
     item5 = types.KeyboardButton('🎲 Рандомний фільм')
     item6 = types.KeyboardButton('🎞 Схожий фільм')
+    item7 = types.KeyboardButton('🎭 Добірка за жанром')
     # додаємо кнопки до клавіатури
-    markup.add(item1, item2, item3, item4, item5, item6)
+    markup.add(item1, item2, item3, item4, item7, item6, item5)
     # виводимо вітальне повідомлення
-    bot.send_message(message.chat.id, "Привіт, <b>{0.first_name}</b>!".format(message.from_user), parse_mode='html', reply_markup = markup)
-
+    bot.send_sticker(message.chat.id, stickers.sticker_hi)
+    bot.send_message(message.chat.id, "Привіт, <b>{0.first_name}</b>! 👋\nЯ ваш персональний помічник у світі кіно - <b>CіneBot</b>.\nЧим саме я можу допомогти?\n\n<i>(Оберіть один з варіантів на клавіатурі)</i>".format(message.from_user), parse_mode='html', reply_markup = markup)
+    
 # обробка клавіатури"
 @bot.message_handler(content_types=['text'])
 def menu(message):
@@ -57,12 +60,13 @@ def menu(message):
     if message.chat.type == 'private':
         #якщо це кнопка "додати запис"
         if message.text == '➕ Додати запис':
-            msg = bot.send_message(message.chat.id, 'Надішліть мені назву фільма і я її запишу!')
+            bot.send_sticker(message.chat.id, stickers.sticker_type)
+            msg = bot.send_message(message.chat.id, 'Надішліть мені назву фільма, я її запишу!')
             # очікує текст від користувача та визиває функцію adddb яка зберігає в БД
             bot.register_next_step_handler(msg,adddb)
         #якщо це кнопка "показати запис"
 
-        if message.text == '📄 Показати записи':
+        elif message.text == '📄 Показати записи':
             user_id = message.from_user.id
             # вибірка даних з бази даних для даного користувача
             cursor.execute(f"SELECT message FROM messages WHERE user_id = {user_id}")
@@ -70,19 +74,22 @@ def menu(message):
             # відправлення результатів користувачу
             if results:
                 message_text = "\n".join([result[0] for result in results])
-                bot.reply_to(message, message_text)
+                bot.send_sticker(message.chat.id, stickers.sticker_hug)
+                bot.send_message(message.chat.id, f"Ваш список збережений фільмів:\n\n{message_text}")
             else:
-                bot.reply_to(message, 'У Вас поки немає записаних фільмів!')
+                bot.send_sticker(message.chat.id, stickers.sticker_question)
+                bot.send_message(message.chat.id, 'У Вас поки немає записаних фільмів.')
         #якщо це кнопка "очистити записи"
 
-        if message.text == '❌ Очистити список':
+        elif message.text == '❌ Очистити список':
             # видаляє дані для конкретного користувача
             cursor.execute(f"DELETE FROM messages WHERE user_id = {message.from_user.id}")
             conn.commit()
-            bot.reply_to(message, "Ваші записи були очищені!")
+            bot.send_sticker(message.chat.id, stickers.sticker_yes)
+            bot.send_message(message.chat.id, "Ваші записи були очищені! ✅")
         #якщо це кнопка "топ 100"
 
-        if message.text == '🎬 Топ-100 фільмів':
+        elif message.text == '🎬 Топ-100 фільмів':
             # отримуємо топ 250 фільмів
             top_movies = ia.get_top250_movies()
             # формування тексту повідомлення
@@ -90,10 +97,11 @@ def menu(message):
             for i in range(100):
                 text += f"{i+1}. {top_movies[i]['title']} ({top_movies[i]['year']})\n"
             # відправлення повідомлення з топ-100 фільмів
+            bot.send_sticker(message.chat.id, stickers.sticker_ride)
             bot.send_message(message.chat.id, text)
         # якщо це кнопка "рандомний фільм"
 
-        if message.text == '🎲 Рандомний фільм':
+        elif message.text == '🎲 Рандомний фільм':
             top_movies = ia.get_top250_movies()
             # повертаємо 1 фідбм з вибірки у 100 фільмів
             random_movie = ia.get_movie(top_movies[random.randint(0, 99)].getID())
@@ -103,20 +111,35 @@ def menu(message):
             genres = ', '.join(random_movie['genres'])
             directors = ', '.join([director['name'] for director in random_movie['directors']])
             response = f'{title} ({year})\n\n'
-            response += f'Жанр: {genres}\n'
+            response += f'Жанр(и): {genres}\n'
             response += f'Режисер(и): {directors}\n'
             imdb_id = random_movie.getID()
             # створюємо інлайн кнопку для переходу до фільму
             markup = types.InlineKeyboardMarkup()
             markup.add(types.InlineKeyboardButton("Відкрити на IMDb", url=f"https://www.imdb.com/title/tt{imdb_id}/"))
+            bot.send_sticker(message.chat.id, stickers.sticker_fun)
+            bot.send_message(message.chat.id, "Ваш випадковий фільм:")
             bot.send_message(message.chat.id, response, reply_markup = markup)
         # якщо це кнопка "схожий фільм"
 
-        if message.text == '🎞 Схожий фільм':
+        elif message.text == '🎞 Схожий фільм':
             # просить вести назву фільму для пошуку
-            msg = bot.send_message(message.chat.id, 'Введіть назву фільму, щоб отримати підбірку з схожими на нього')
+            bot.send_sticker(message.chat.id, stickers.sticker_type)
+            msg = bot.send_message(message.chat.id, 'Введіть назву фільму, щоб отримати добірку фільмів схожих на нього')
             # визиває функцію з файлу ai
-            bot.register_next_step_handler(msg,ai.Rec.airec)
+            bot.register_next_step_handler(msg,ai.AiRec.rec_name)
+        
+        elif message.text == '🎭 Добірка за жанром':
+            # просить вести назву фільму для пошуку
+            bot.send_sticker(message.chat.id, stickers.sticker_type)
+            msg = bot.send_message(message.chat.id, 'Введіть жанр за яким хочете отримати добірку')
+            # визиває функцію з файлу ai
+            bot.register_next_step_handler(msg,ai.AiRec.rec_genre)
+
+        else:
+            oops = "Вибачте, я не знаю таку команду :("
+            bot.send_sticker(message.chat.id, stickers.sticker_question)
+            bot.send_message(message.chat.id, oops)
 
 # функція яка додає дані до БД
 def adddb(message):
@@ -125,7 +148,8 @@ def adddb(message):
     # Додавання даних до бази даних
     cursor.execute(f"INSERT INTO messages (user_id, message) VALUES ({user_id}, '{message_text}')")
     conn.commit()
-    bot.reply_to(message, 'Я записав Ваш фільм!')
+    bot.send_sticker(message.chat.id, stickers.sticker_fun)
+    bot.reply_to(message, 'Я записав ваш фільм! ✅')
 
 # запуск бота
 bot.polling(none_stop = True)
